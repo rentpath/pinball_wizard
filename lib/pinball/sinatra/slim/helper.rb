80e325c633efbefa99dd77caf52da14e29e49025
Caleb Wright <awright@rentpath.com>
2014-06-20 12:11:17 -0400

    [CW] [73045828] Rename Sinatra/Slim Helper

---
+++ lib/pinball/sinatra/slim/helper.rb
@@ -0,0 +1,15 @@
+module Pinball
+  module Sinatra
+    module Slim
+      module Helper
+        def feature(name, opts_or_locals = {})
+          if Pinball::Registry.available?(name)
+            partial_name = opts_or_locals.fetch(:partial, name)
+            locals = opts_or_locals.tap { |h| h.delete(:partial) }
+            partial "features/#{partial_name}", locals
+          end
+        end
+      end
+    end
+  end
+end
