80d757f8bfaf5325b626b624b174e405e8779735
Caleb Wright <awright@rentpath.com>
2014-06-26 11:03:53 -0400

    [CW/CR] [73548898] Simplify Rails Helper Options

--- lib/pinball_wizard/rails/helper.rb
+++ lib/pinball_wizard/rails/helper.rb
@@ -1,11 +1,11 @@
 module PinballWizard
   module Rails
     module Helper
-      def feature(name, opts_or_locals = {})
+      def feature(name, options = {})
         if PinballWizard::Registry.available?(name)
-          partial_name = opts_or_locals.fetch(:partial, name)
-          locals = opts_or_locals.tap { |h| h.delete(:partial) }
-          render partial: "features/#{partial_name}", locals: locals
+          partial_name = options.fetch(:partial) { name }
+          options[:partial] = "features/#{partial_name}"
+          render options
         end
       end
     end
